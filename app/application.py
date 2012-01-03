13ceebfb81ee58f180b5a78af3050dbbf159ecfc
Sean M. Vieira <thelastfool@gmail.com>
2012-01-02 19:37:43 -0500

    Use a better mapping pattern

--- app/application.py
+++ app/application.py
@@ -1,4 +1,4 @@
-from flask import abort, flash, Flask, g, Markup, redirect, render_template, request, url_for
+from flask import abort, flash, Flask, g, jsonify, Markup, redirect, render_template, request, url_for
 from flask.ext import admin
 from importer import FieldMappingForm, FileUploadForm, load_from_file
 from models import db
@@ -7,7 +7,9 @@ from os import path
 from re import compile, IGNORECASE
 from server.database import connect_db, query_db
 from werkzeug import secure_filename
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.fields import SelectField
+from wtforms.validators import Required
 
 
 VALID_TABLENAME = compile("^[a-z]+[a-z0-9_]$", IGNORECASE)
@@ -71,20 +73,28 @@ def configure_routes(app, db):
         if request.method == "POST" and form.validate():
             f = request.files["file"]
             filename = secure_filename(f.filename)
-            f.save(path.join(app.config["UPLOAD_FOLDER"], filename))
-            return redirect(url_for("data_mapping", filename=filename))
+            if filename is None or not filename:
+                flash("Please provide a file", category="error")
+            else:
+                f.save(path.join(app.config["UPLOAD_FOLDER"], filename))
+                return redirect(url_for("data_mapping", filename=filename, mode=form.mode.data))
 
         return render_template("edit_layout.html", form=form, title="Import Data")
 
     @app.route("/import/<path:filename>", methods=["GET", "POST"])
     def data_mapping(filename=None):
-        f = open(path.join(app.config["UPLOAD_FOLDER"], filename), "r")
+        try:
+            f = open(path.join(app.config["UPLOAD_FOLDER"], filename), "r")
+        except IOError:
+            return abort(404)
+
         f = load_from_file(f)
         headers = next(f)
         lowercase_headers = [h.lower() for h in headers]
         headers_ = [""] + headers
         header_list = zip(headers_, headers_)
         table_columns = Transaction.__table__.columns
+        mode = request.args.get("mode", None)
 
         class NewForm(FieldMappingForm):
             pass
@@ -93,16 +103,15 @@ def configure_routes(app, db):
             if not field.primary_key:
                 i = lowercase_headers.index(field.name.lower()) if field.name.lower() in lowercase_headers else -1
                 default_ = headers[i] if i >= 0 else None
-                setattr(NewForm, field.name, SelectField(field.name, choices=header_list, default=default_))
+                NewForm = _generate_field(NewForm, field, choices=header_list, default=default_, mode=mode)
 
         form = NewForm(request.form)
 
         if request.method == "POST" and form.validate():
             results = {}
             for field in form:
-                if field.data and field.name in table_columns:
-                    results[field.name] = field.data
-            return str(results)
+                results[field.name] = str(field.data)
+            return jsonify(**results)
 
         return render_template("edit_layout.html", form=form, title="Map Fields")
 
@@ -127,3 +136,26 @@ def configure_routes(app, db):
                                     title="Table View for {}".format(table_name))
 
     return app
+
+
+def _generate_field(form, db_field, choices=None, default=None, mode=None):
+    if db_field.name.lower() == "transactiontypeid":
+        income_transactions = lambda val: lambda: TransactionType.query.filter(TransactionType.isIncome == val)
+        income_field = QuerySelectField("Map income values to", query_factory=income_transactions(1), allow_blank=True, validators=[Required()])
+        spend_field = QuerySelectField("Map spending values to", query_factory=income_transactions(0), allow_blank=True, validators=[Required()])
+        setattr(form, "income_transactions", income_field)
+        setattr(form, "spend_transactions", spend_field)
+        return form
+    elif db_field.name.lower() == "amount" and mode == "seperate":
+        income_column = SelectField("Field containing income values", choices=choices, default=default)
+        spend_column = SelectField("Field containing spending values", choices=choices, default=default)
+        setattr(form, "income_column", income_column)
+        setattr(form, "spend_column", spend_column)
+        return form
+    elif db_field.name.lower() == "accountid":
+        field = QuerySelectField("Account", query_factory=lambda: Account.query, allow_blank=True, validators=[Required("Please select an account")])
+    else:
+        field = SelectField(db_field.name, choices=choices, default=default)
+
+    setattr(form, db_field.name, field)
+    return form
--- app/importer.py
+++ app/importer.py
@@ -1,19 +1,18 @@
 import csv
 from itertools import izip_longest
-from models import Account, db, Transaction
+from models import db, Transaction
 from wtforms import Form
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.fields import FileField, SubmitField
+from wtforms.fields import FileField, SelectField, SubmitField
 from wtforms.validators import Required
 
 
 class FileUploadForm(Form):
     file = FileField("Data file")
+    mode = SelectField("File Type", choices=[("", ""), ("mixed", "Intermixed save and spend"), ("seperate", "Seperate save and spend columns")], validators=[Required()])
     action = SubmitField("Upload")
 
 
 class FieldMappingForm(Form):
-    account = QuerySelectField("Account", query_factory=lambda: Account.query, allow_blank=True, validators=[Required("Please select an account")])
     action = SubmitField("Import")
 
 
@@ -29,7 +28,7 @@ def load_from_file(f, skip_lines=0, **csv_options):
 
 def import_data(rows, field_names=None, Model=Transaction, session=None):
     field_names = field_names if field_names else []
-    session = session if session else db.session
+    session = session if session is not None else db.session
 
     for row in rows:
         session.add(Model(**dict(izip_longest(field_names, row, fillvalue=None))))
