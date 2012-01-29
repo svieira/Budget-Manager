6b4806240117e4a2caace81249ab2ffd3320bb05
Sean M. Vieira <thelastfool@gmail.com>
2012-01-28 21:47:46 -0500

    Add basic management script

---
+++ app/manage.py
@@ -0,0 +1,19 @@
+from application import create_app
+from models.base_model import db
+from flaskext.script import Manager
+
+app = create_app()
+command_runner = Manager(app)
+
+
+@command_runner.command
+def create_db():
+    """Creates the necessary tables in the database
+    specified in the `DB_PATH` config variable."""
+
+    print "Initializing budget tables ..."
+    db.metadata.create_all(db.engine)
+
+
+if __name__ == "__main__":
+    command_runner.run()
