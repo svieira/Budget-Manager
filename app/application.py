b08e28aba4add3a160ac3d0926565b7dfe1c8297
Sean M. Vieira <thelastfool@gmail.com>
2012-02-11 21:21:40 -0500

    Add links in search results

--- app/application.py
+++ app/application.py
@@ -151,13 +151,25 @@ def configure_routes(app, db):
                     search_terms.append(term)
             return [getattr(model, term).like(query) for term in search_terms]
 
+        def generate_links(query_list, model=None, model_key=None):
+            link = Markup("""<a href="{}">{}</a> - <a href="{}">Delete</a>""")
+            model_key = [key.key for key in model_key.columns][0]
+            for entry in query_list:
+                key = getattr(entry, model_key, 1)
+                name = model.__name__
+                edit_url = url_for('admin.edit', model_name=name, model_url_key=key)
+                delete_url = url_for('admin.delete', model_name=name, model_url_key=key)
+                yield link.format(edit_url, entry, delete_url)
+
         flash("""Searching for "{}" ...""".format(query))
         if query.count("%") == 0:
             query = "%{}%".format(query)
         if model is None:
             for model_name, model in MANAGED_MODEL_MAP.items():
                 search_terms = generate_search_terms(model)
-                results.append((model_name, db.session.query(model).filter(or_(*search_terms)).all()))
+                data = db.session.query(model).filter(or_(*search_terms)).all()
+                data = list(generate_links(data, model, model.__table__.primary_key))
+                results.append((model_name, data))
             return render_template("search.html", results=results)
         else:
             if not model.lower() in MANAGED_MODEL_MAP:
